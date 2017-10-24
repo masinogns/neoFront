eadb10d119f6bd9fe9d57640400fd1a89f5719dc
백기훈 <masinogns@gmail.com>
2017-10-24 17:10:41 +0900

    detail list and map toggle finish

--- js/detail.js
+++ js/detail.js
@@ -9,3 +9,39 @@ function initMap() {
     map: map
   });
 }
+
+var content = document.querySelector(".content");
+
+var tag = document.querySelector(".nav");
+tag.addEventListener('click', activeNav);
+
+function activeNav(e){
+  var findIndexOfActive = 0;
+
+  for (var i = 0; i < tag.childElementCount; i++) {
+    if (tag.children[i].className == "active") {
+      findIndexOfActive = i;
+    }
+  }
+
+  var target = e.target;
+  var parent = e.target.parentNode;
+
+  if (target.className == "map") {
+    console.log("map is clicked");
+    content.children["list"].style.height = "0px";
+    content.children["map"].style.height = "300px";
+
+  }else { // if target == enter
+    console.log("list is clikced");
+    content.children["list"].style.height = "300px";
+    content.children["map"].style.height = "0px";
+  }
+
+  changeActive(parent, findIndexOfActive);
+}
+
+function changeActive(parent, index){
+  tag.children[index].className = "";
+  parent.className = "active";
+}
