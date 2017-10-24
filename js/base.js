3a74a1b1fb02eb901cb010e9350f3feb86288228
leeikjeun <dldlrwjs12@gmail.com>
2017-10-24 16:23:21 +0900

    make menu toggle

--- js/base.js
+++ js/base.js
@@ -5,6 +5,8 @@ var mainTitleBox = document.querySelector(".main-title");
 var initConentList = document.querySelectorAll(".content-list");
 var settingHeight = document.querySelector("body").offsetHeight - document.querySelector("header").offsetHeight;
 
+//--------밑에 메뉴 3등분으로 나누는 코드
+
 mainTitle.style.paddingTop = (mainTitleBox.offsetHeight/2 - mainTitle.offsetHeight/2) + "px";
 menuIcon.style.marginTop = (mainTitleBox.offsetHeight/2 - menuIcon.offsetHeight/2) + "px";
 
@@ -12,3 +14,20 @@ menuIcon.style.marginTop = (mainTitleBox.offsetHeight/2 - menuIcon.offsetHeight/
  for(var i = 0; i < initConentList.length; i++){
      initConentList[i].style.height = (settingHeight/3) + "px";
  }
+
+//------밑에 코드 메뉴 토글 동작코드
+
+ var menuModalList = document.querySelector(".menu-modal-list");
+ var menuModal = document.querySelector(".menu-modal");
+ var menuBtn = document.querySelector(".menu-item");
+
+
+
+ menuBtn.addEventListener('click',function(e){
+   menuModal.style.height = "calc(100vh - 10rem)"
+ },false);
+
+ menuModal.addEventListener('click',function(e){
+   if(e.target.className == "menu-modal")
+     menuModal.style.height = "0px"
+ },true)
