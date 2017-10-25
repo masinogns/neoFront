6fbf1da624c049cbbe15c621cd1b5273a441a9fe
백기훈 <masinogns@gmail.com>
2017-10-25 10:23:39 +0900

    add reference url in detailSlider.js

--- js/detailSlider.js
+++ js/detailSlider.js
@@ -1,3 +1,5 @@
+/*https://www.w3schools.com/howto/howto_js_rangeslider.asp*/
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
