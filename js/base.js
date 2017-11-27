295230633b3fb4cb427f22b337226c16c73ab13c
백기훈 <masinogns@gmail.com>
2017-11-27 17:45:05 +0900

    change main

--- js/base.js
+++ js/base.js
@@ -1,19 +1,3 @@
-var mainTitle = document.querySelector(".title");
-var menuIcon = document.querySelector(".menu-item");
-var mainTitleBox = document.querySelector(".main-title");
-
-var initConentList = document.querySelectorAll(".content-list");
-var settingHeight = document.querySelector("body").offsetHeight - document.querySelector("header").offsetHeight;
-
-//--------밑에 메뉴 3등분으로 나누는 코드
-
-mainTitle.style.paddingTop = (mainTitleBox.offsetHeight/2 - mainTitle.offsetHeight/2) + "px";
-menuIcon.style.marginTop = (mainTitleBox.offsetHeight/2 - menuIcon.offsetHeight/2) + "px";
-
-
- for(var i = 0; i < initConentList.length; i++){
-     initConentList[i].style.height = (settingHeight/3) + "px";
- }
 
 //------밑에 코드 메뉴 토글 동작코드
 
